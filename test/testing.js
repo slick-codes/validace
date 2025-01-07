a17d0b2e2880eff379aa80d1c60121348bdfb20c
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-01-07 11:46:01 +0100

    add test file

--- test/testing.js
+++ test/testing.js
@@ -1,40 +1,38 @@
+const Validace = require('../src/index')
 
-
-const Validace = require("../src/index")
-
-console.clear();
-console.clear();
+console.clear()
+console.clear()
+console.log()
 console.log()
 console.log()
 
 // set schema properties
 const schema = new Validace({
-    firstName: { type: "string", required: true, /* allowEmptyString: true */ },
-    lastName: { type: "string", required: true },
-    surName: { type: "string", required: true, allowEmptyString: [false, " (%key%) should not be empty"] },
-    age: { type: "number", required: true },
-    email: { type: "email", required: true }
+    firstName: { type: 'string', required: true /* allowEmptyString: true */ },
+    lastName: { type: 'string', required: true },
+    surName: { type: 'string', required: true, allowEmptyString: [false, ' (%key%) should not be empty'] },
+    age: { type: 'number', required: true },
+    email: { type: 'email', required: true },
 })
 
 // set configuration
 schema.config({
     allowUnregisteredKeys: false,
-    allowEmptyString: false // this prevents any empty string, note: this will be overwritten if you use the allowEmptyString inside the schema field
+    allowEmptyString: false, // this prevents any empty string, note: this will be overwritten if you use the allowEmptyString inside the schema field
 })
 
 // validate data
 const result = schema.validate({
-    firstName: "",
-    lastName: "Ezekiel-Hart",
-    surName: "",
+    firstName: '',
+    lastName: 'Ezekiel-Hart',
+    surName: '',
     age: 45,
-    email: "hartpaulisimo@gmail.com",
-    hobbies: "Coding"
+    email: 'hartpaulisimo@gmail.com',
+    hobbies: 'Coding',
 })
-
-
 
 console.log(result)
 
 console.log()
 console.log()
+
